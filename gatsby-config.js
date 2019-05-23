fef4b2d1146bc0ff619d1063b95c6956db83e7c5
Ryan Bateman <rybateman@gmail.com>
2019-05-23 15:39:30 -0800

    01 section steps completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby + Drupal 8`,
+    slogan: `A Match Made in Heaven`,
+    description: `A demo site made in our Gatsby + Drupal training session at Drupal Camp Chatanooga 2019!`,
+    author: `nerdstein`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
