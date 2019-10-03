9b5f99d33abf424bc7e6bc1ae3e745bbaaa6807a
Ryan Bateman <rybateman@gmail.com>
2019-10-02 23:33:06 -0700

    gatsby-config.js boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-source-drupal`,
       options: {
-        baseUrl: `https://d082p.ply.st`, // use your simplytest url here
+        baseUrl: `https://stm5d95917bef2e4-l6po7xb9wogqpizeor0rmtsio9o6wfcc.tugboat.qa/`, // use your simplytest url here
         apiBase: `jsonapi`, // optional, defaults to `jsonapi`
       },
     },
